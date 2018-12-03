ef8a78ab2bebc469529b371c66195242afc7814d
kenjishore <shorekenji@gmail.com>
2018-12-03 09:38:35 -0500

    linked SDL2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include <GL/glew.h>
+#include <SDL2/SDL.h>
 
 int main() {
-    std::cout << "hi, World!" << std::endl;
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cout << "SDL could not initialize! SDL Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    std::cout << "success!" << std::endl;
 
     return 0;
 }
