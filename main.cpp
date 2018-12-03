3c7f0e14fcdffb25aa6bfdfa53c8d3a2f1a30a71
kenjishore <shorekenji@gmail.com>
2018-12-03 10:12:05 -0500

    linked SDL2

--- main.cpp
+++ main.cpp
@@ -3,13 +3,49 @@
 #include <SDL2/SDL.h>
 
 int main() {
-    if(SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
+    bool quit = false;
+    SDL_Window* window;
+    SDL_GLContext glContext;
+    SDL_Event sdlEvent;
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cout << "SDL could not initialize! SDL Error: " << SDL_GetError() << std::endl;
         return -1;
     }
 
-    std::cout << "success!" << std::endl;
+    window = SDL_CreateWindow("Test Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 600, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+    if (window == NULL) {
+        std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    glContext = SDL_GL_CreateContext(window);
+    if (glContext == NULL) {
+        std::cout << "OpenGL context could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    glewInit();
+    while (!quit) {
+        while (SDL_PollEvent(&sdlEvent) != 0) {
+            if (sdlEvent.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+
+        glClearColor(1, 0, 0, 1);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        SDL_GL_SwapWindow(window);
+    }
+
+    SDL_DestroyWindow(window);
+    window = NULL;
+
+    SDL_Quit();
 
     return 0;
 }
